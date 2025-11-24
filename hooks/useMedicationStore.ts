c53c556f777139142671010a11b277a6726b9e44
israelbernardo22 <ib94534@unifei.edu.br>
2025-11-24 02:45:11 +0000

    Ajuste remocao gemini e erro compilacao

--- hooks/useMedicationStore.ts
+++ hooks/useMedicationStore.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { Medication as MedicationType, HistoryEntry, Profile } from '../types';
+import { HistoryEntry, Profile } from '../types';
 
 
 // Classe Medication
